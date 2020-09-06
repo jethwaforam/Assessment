2dceafab9db6274d1d0bd6854d969cfea9d8f4a5
jethwaforam@gmail.com <jethwaforam@gmail.com>
2020-09-07 00:16:08 +0530

    Added Products & ProductOffers models

---
+++ PromotionEngineApi/Model/ProductOffers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngineApi.Model
+{
+    public class ProductOffers    
+    {
+        public int BaseProductId { get; set; }
+
+        public List<Products> Products { get; set; }
+
+        public int ProductQuantity { get; set; }
+
+        public decimal OfferPrice { get; set; }
+    }
+}
---
+++ PromotionEngineApi/Model/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngineApi.Model
+{
+     public class Products
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
